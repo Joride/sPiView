21f2e8b26b872908821587d8410cdde8549de22c
Joride <kerrelinc@gmail.com>
2018-01-06 14:07:40 +0100

    requesting push notification

--- sPiView/src/AppDelegate.m
+++ sPiView/src/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+@import UserNotifications;
 
 @interface AppDelegate ()
 
@@ -17,6 +18,34 @@
 - (BOOL)application:(UIApplication *)application
 didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+    [application registerForRemoteNotifications];
+    UNUserNotificationCenter* center =
+    [UNUserNotificationCenter currentNotificationCenter];
+    [center requestAuthorizationWithOptions:(UNAuthorizationOptionAlert + UNAuthorizationOptionSound)
+                          completionHandler:^(BOOL granted,
+                                              NSError * _Nullable error)
+     {
+         // Enable or disable features based on authorization.
+     }];
     return YES;
+}
+
+-(void)application:(UIApplication *)application
+didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
+{
+    NSMutableString * token = [NSMutableString string];
+    const char      * data  = [deviceToken bytes];
+    
+    for (NSUInteger i = 0; i < [deviceToken length]; i++)
+    {
+        [token appendFormat:@"%02.2hhX", data[i]];
+    }
+    
+    DebugLog(@"\t\t==== PUSHTOKEN: '%@' ====\n\n", token);
+}
+- (void)application:(UIApplication *)application
+didFailToRegisterForRemoteNotificationsWithError:(NSError *)error
+{
+    
 }
 @end
